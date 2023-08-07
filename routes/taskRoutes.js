bd37a944fb90711695ce9077fa41a2b9027aa1fa
arslanzaki <arslanzaki223@gmail.com>
2023-08-07 13:19:21 +0500

    Added GetTask & GetTaskByID

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -11,6 +11,8 @@ router.get("/test", auth, (req, res) => {
 });
 
 // CRUD Tasks For Authenticated Users
+
+// Create A Task
 router.post("/", auth, async (req, res) => {
   try {
     // description, completed -> from req.body
@@ -30,4 +32,40 @@ router.post("/", auth, async (req, res) => {
     res.status(400).json({ error: err });
   }
 });
+
+// Get User Tasks
+router.get("/", auth, async (req, res) => {
+  try {
+    const tasks = await Task.find({
+      owner: req.user._id,
+    });
+    res.status(200).json({
+      tasks,
+      count: tasks.length,
+      message: "Tasks Fetched Successfully!",
+    });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+// Get Own Task By Id
+router.get("/:id", auth, async (req, res) => {
+  const taskid = req.params.id;
+
+  try {
+    const task = await Task.findOne({
+      _id: taskid,
+      owner: req.user._id,
+    });
+
+    if (!task) {
+      return res.status(404).json({ message: "Task Not Found" });
+    }
+    res.status(200).json({ task, message: "Task Fetched Successfully!" });
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
 module.exports = router;
